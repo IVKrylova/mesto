97be53af4891832c0695bb50bcf656628a018f39
IVKrylova <ivk.lis@yandex.ru>
2021-10-28 23:50:58 +0300

    feat: доработать вставку значений в input__name и input__profession из profile

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,14 +1,24 @@
 const popup = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.button-edit');
 const buttonClose = document.querySelector('.button-close');
+const profileName = document.querySelector('.profile__name');
+const inputName = document.querySelector('#name');
+const profileProfession = document.querySelector('.profile__profession');
+const inputProfession = document.querySelector('#profession');
+
+//console.log(inputName);
+
 
 function popupOpened() {
- popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
 }
 
 function popupRemoved() {
   popup.classList.remove('popup_opened');
 }
 
+// открытие и закрытие popup
 buttonEdit.addEventListener('click', popupOpened);
 buttonClose.addEventListener('click', popupRemoved);
