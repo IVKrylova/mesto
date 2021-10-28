ba2dc71c3d9d58958d493e58eec63f0b81879c04
IVKrylova <ivk.lis@yandex.ru>
2021-10-29 00:13:08 +0300

    feat: добавить возможность сохранения изменений при отправке формы

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,24 +1,36 @@
-const popup = document.querySelector('.popup');
+const popupElement = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.button-edit');
 const buttonClose = document.querySelector('.button-close');
 const profileName = document.querySelector('.profile__name');
 const inputName = document.querySelector('#name');
 const profileProfession = document.querySelector('.profile__profession');
 const inputProfession = document.querySelector('#profession');
+const formElement = document.querySelector('.form');
+const formButton = document.querySelector('.form__button');
 
-//console.log(inputName);
-
-
+// функция открытия popup
 function popupOpened() {
-  popup.classList.add('popup_opened');
+  popupElement.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
 
-function popupRemoved() {
-  popup.classList.remove('popup_opened');
+// функция закрытия popup
+function popupClouse() {
+  popupElement.classList.remove('popup_opened');
+}
+
+// oбработчик отправки формы
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputProfession.value;
 }
 
 // открытие и закрытие popup
 buttonEdit.addEventListener('click', popupOpened);
-buttonClose.addEventListener('click', popupRemoved);
+buttonClose.addEventListener('click', popupClouse);
+
+// прикрепляем обработчик к форме и закрываем форму при нажатии на кнопку 'сохранить'
+formElement.addEventListener('submit', formSubmitHandler);
+formButton.addEventListener('click', popupClouse);
