7f927f47d7aa07b8dd525f2e3190efb7481c14e7
IVKrylova <ivk.lis@yandex.ru>
2021-11-10 01:45:24 +0300

    feat: написать  добавить в блок popup для добавления карточки в elements

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,35 +1,56 @@
-const popupElement = document.querySelector('.popup');
+const popupProfileInfo = document.querySelector('#popup-profile-info');
 const buttonEdit = document.querySelector('.button-edit');
-const buttonClose = document.querySelector('.button-close');
+const buttonClosePopupProfileInfo = document.querySelector('#button-close-popup-profile-info');
 const profileName = document.querySelector('.profile__name');
 const inputName = document.querySelector('#name');
 const profileProfession = document.querySelector('.profile__profession');
 const inputProfession = document.querySelector('#profession');
 const formElement = document.querySelector('.form');
+const buttonAdd = document.querySelector('.button-add');
+const popupElementCard = document.querySelector('#popup-element-card');
+const buttonClosePopupElementCard = document.querySelector('#button-close-popup-element-card');
 
-// функция открытия popup
-function popupOpened() {
-  popupElement.classList.add('popup_opened');
+// функция открытия popup в profile__info
+function popupProfileInfoOpened() {
+  popupProfileInfo.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
 
-// функция закрытия popup
-function popupClouse() {
-  popupElement.classList.remove('popup_opened');
+// функция закрытия popup в profile__info
+function popupProfileInfoClouse() {
+  popupProfileInfo.classList.remove('popup_opened');
 }
 
-// oбработчик отправки формы
+// функция открытия popup для добавления карточки в elements
+function popupElementCardOpened() {
+  popupElementCard.classList.add('popup_opened');
+}
+
+// функция закрытия popup для добавления карточки в elements
+function popupElementCardClouse() {
+  popupElementCard.classList.remove('popup_opened');
+}
+
+// открытие popup в profile__info
+buttonEdit.addEventListener('click', popupProfileInfoOpened);
+
+// закрытие popup в profile__info
+buttonClosePopupProfileInfo.addEventListener('click', popupProfileInfoClouse);
+
+// oбработчик отправки формы в profile__info
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileProfession.textContent = inputProfession.value;
-  popupClouse();
+  popupProfileInfoClouse();
 }
 
-// открытие и закрытие popup
-buttonEdit.addEventListener('click', popupOpened);
-buttonClose.addEventListener('click', popupClouse);
+// прикрепляем обработчик к форме в profile__info
+popupProfileInfo.addEventListener('submit', formSubmitHandler);
 
-// прикрепляем обработчик к форме
-formElement.addEventListener('submit', formSubmitHandler);
+// открытие popup для добавления карточки в elements
+buttonAdd.addEventListener('click', popupElementCardOpened);
+
+// закрытие popup для добавления карточки в elements
+buttonClosePopupElementCard.addEventListener('click', popupElementCardClouse);
