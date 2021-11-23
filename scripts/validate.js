6f29856dda689589107d4c449d0b6c6f1df40672
IVKrylova <ivk.lis@yandex.ru>
2021-11-24 00:27:34 +0300

    feat: добавить функцию переключения кнопки при проверке валидности полей формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,19 +1,3 @@
-/* // включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.form__item',
-  submitButtonSelector: '.form__button',
-  inactiveButtonClass: 'form__button_disabled',
-  inputErrorClass: 'form__item_type_error',
-  errorClass: 'form__error_visible'
-}); */
-
-// переменные, необходимые валидации формы в в profile__info
-const formProfileInfo = document.querySelector('#form-edit-profile');
-const inputFormProfileInfo = formProfileInfo.querySelector('.form__item');
-
 // функция добавления класса с ошибкой к input
 function showInputError(formElement, inputElement, errorMessage) {
   // выбор элемента ошибки на основе уникального класса
@@ -46,14 +30,19 @@ function isValid(formElement, inputElement) {
 // функция добавления обработчиков всем полям формы
 function setEventListeners(formElement) {
   const inputsList = Array.from(formElement.querySelectorAll('.form__item'));
+  const buttonElement = formElement.querySelector('.form__button');
+
+  toggleButtonState(inputsList, buttonElement);
 
   inputsList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
+      toggleButtonState(inputsList, buttonElement);
     });
   });
 }
 
+// функция добавления обработчиков всем формам
 function enableValidation() {
   const formsList = Array.from(document.querySelectorAll('.form'));
 
@@ -62,6 +51,29 @@ function enableValidation() {
   });
 }
 
+// функция проверки валидности всех полей формы
+function hasInvalidInput(inputsList) {
+  return inputsList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+// функция переключения кнопки
+function toggleButtonState(inputsList, buttonElement) {
+  if (hasInvalidInput(inputsList)) {
+    buttonElement.classList.add('form__button_disabled');
+  } else {
+    buttonElement.classList.remove('form__button_disabled');
+  }
+};
 
 
-enableValidation()
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.form__item',
+  submitButtonSelector: '.form__button',
+  inactiveButtonClass: 'form__button_disabled',
+  inputErrorClass: 'form__item_type_error',
+  errorClass: 'form__error_visible'
+});
